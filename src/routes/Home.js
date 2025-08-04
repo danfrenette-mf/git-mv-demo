59502c4d3287c46080461e94d406cde0a92d2642
Dan Frenette <dan.frenette@marketfuel.com>
2025-08-03 20:23:44 -0400

    feature: add user greeting to Home component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,16 @@
 // Home Component
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useUser } from '../hooks/useUser';
 
 function Home() {
+  const user = useUser();
+  
   return (
     <div>
-      <h1>Welcome Home</h1>
+      <h1>Welcome Home{user ? `, ${user.name}` : ''}!</h1>
       <p>This is the home page.</p>
+      <p>You are logged in as: {user?.name || 'Guest'}</p>
       <Link to="/dashboard">Go to Dashboard</Link>
     </div>
   );
